99322a47ec2111451b6d3d5580d3656882b65b0c
YUJING GAO <gao.yujing.csu@gmail.com>
2019-10-18 05:59:49 +0000

    change style

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get('/random_comic', function(req, res) {
 });
 
 app.post('/get_random_comic', function(req,res){
-    res.redirect('/');
+    res.redirect('/random_comic');
 });
 
 http.createServer(app).listen(port, function() {
