83c2b579de277bce78640abbd5ddfab67959f1f3
YUJING GAO <gao.yujing.csu@gmail.com>
2019-10-18 04:18:27 +0000

    s

--- app.js
+++ app.js
@@ -30,26 +30,6 @@ app.get('/random_comic', function(req, res) {
     res.render("random_comic");
 });
 
-app.post('/addtask', function(req, res) {
-    var newTask = req.body.newtask;
-    task.push(newTask);
-    res.redirect('/');
-});
-
-app.post('/removetask', function(req, res) {
-    var completeTask = req.body.check;
-    if (typeof completeTask === "string") {
-        complete.push(completeTask);
-        task.splice(task.indexOf(completeTask), 1);
-    }
-    else if (typeof completeTask === "object") {
-        for (var i = 0; i < completeTask.length; i++) {
-            complete.push(completeTask[i]);
-            task.splice(task.indexOf(completeTask[i]), 1);
-        }
-    }
-    res.redirect('/');
-});
 
 http.createServer(app).listen(port, function() {
 
