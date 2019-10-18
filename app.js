7c5878e0806592bcdc7f2d5a8ad9bfdb71b576dc
YUJING GAO <gao.yujing.csu@gmail.com>
2019-10-18 04:49:23 +0000

    internal error test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var path = require('path');
+var request = require('request');
 const express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
@@ -15,28 +16,21 @@ var task = ["clean", "cook"];
 var complete = ["eat", "sleep"];
 
 app.get('/', function(req, res) {
-    // var request = new XMLHttpRequest();
-    // var imgURL;
-    // request.open('GET', 'https://xkcd.com/info.0.json', true);
-    // request.onload = function() {
-    //     var data = JSON.parse(this.response)
-    //     imgURL = data.img;
-    // };
-    // request.send();
-    var xmlhttp = new XMLHttpRequest();
+    var url = "https://xkcd.com/info.0.json";
     var img_url;
+    request({
+        url: url,
+        json: true
+    }, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var object = Json.parse(body);
+            img_url = object.img;
+        }
+    });
+
     //var title;
     //var year;
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var myObj = JSON.parse(this.responseText);
-            img_url = myObj.img;
-            //title = myObj.title;
-            //year = myObj.year;
-        }
-    };
-    xmlhttp.open("GET", "https://xkcd.com/info.0.json", true);
-    xmlhttp.send();
+
     //var url = "https://imgs.xkcd.com/comics/percent_milkfat.png";
     res.render("index", { img_url: img_url});
 });
